ef1e9e3f659ee9b5c0b42484168617376d667477
Vanessa McHale <vamchale@gmail.com>
2018-04-07 20:23:19 -0500

    hlint

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,16 +1,16 @@
-{-# LANGUAGE OverloadedStrings #-} 
+{-# LANGUAGE OverloadedStrings #-}
 
-import Data.QRCodes
-import Test.Hspec
+import           Data.QRCodes
+import           Test.Hspec
 
 main :: IO ()
-main = hspec $ do
+main = hspec $
   describe "Data.QRCodes" $ do
-    it "writes a QR code" $ do
-      byteStringToQR "hello world" "qrcode.png" >>= (`shouldBe` ())
-    it "reads a QR code" $ do
-      readQRString "qrcode.png" >>= (`shouldBe` "hello world")
-    it "writes a secure QR code" $ do
-      createSecureQRCode ("small" :: String) ".key.hk" "qrcode-sec.png" >>= (`shouldBe` ())
-    it "reads a secure QR code" $ do
-      (readQRStrSec "qrcode-sec.png" ".key.hk" :: IO String) >>= (`shouldBe` "small")
+  it "writes a QR code" $
+    byteStringToQR "hello world" "qrcode.png" >>= (`shouldBe` ())
+  it "reads a QR code" $
+    readQRString "qrcode.png" >>= (`shouldBe` "hello world")
+  it "writes a secure QR code" $
+    createSecureQRCode ("small" :: String) ".key.hk" "qrcode-sec.png" >>= (`shouldBe` ())
+  it "reads a secure QR code" $
+    (readQRStrSec "qrcode-sec.png" ".key.hk" :: IO String) >>= (`shouldBe` "small")
